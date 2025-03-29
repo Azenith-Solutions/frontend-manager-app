63da2d0f2bdb776e5b376fe99130d1a53e6fe96a
jeancharlesx64 <jean.albuquerque@sptech.school>
2025-03-29 14:38:09 -0300

    [FEAT] :: kinda logout system

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () =>{
+        const wantsToLogout = window.confirm("Você realmente deseja sair?");
+        if(wantsToLogout){
+            localStorage.removeItem('isLoggedIn');
+            navigate('/');
+        }
+    }
+
     return(
         <>
             <h1>Dashboard</h1>
+            <button onClick={handleLogout}>Logout</button>
         </>
     )
 }
