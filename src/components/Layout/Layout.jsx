68678228327aa1bbeb8409a93dc4b6e3f7e15bfe
jeancharlesx64 <jean.albuquerque@sptech.school>
2025-05-08 22:42:59 -0300

    [FEAT] :: adding header user details, name, notification and recent emails

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -17,7 +17,9 @@ import {
     useMediaQuery,
     useTheme,
     Collapse,
-    Tooltip
+    Tooltip,
+    Avatar,
+    InputBase
 } from "@mui/material";
 
 import {
@@ -28,8 +30,13 @@ import {
     ChevronLeft as ChevronLeftIcon,
     ChevronRight as ChevronRightIcon,
     ArrowBackIosNew as ArrowBackIosNewIcon,
-    ArrowForwardIos as ArrowForwardIosIcon
+    ArrowForwardIos as ArrowForwardIosIcon,
+    NotificationsNoneOutlined as NotificationsNoneOutlinedIcon,
+    MailOutlineOutlined as MailOutlineOutlinedIcon,
+    Search as SearchIcon
 } from "@mui/icons-material";
+
+import { alpha } from '@mui/material/styles';
 
 // icons
 import Logo from "../../assets/Logo.svg";
@@ -429,9 +436,63 @@ const Layout = () => {
                     >
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="h4" noWrap component="div" sx={{ flexGrow: 1 }}>
+                    <Typography variant="h5" noWrap component="div" sx={{ flexGrow: 1 }}>
                         {getPageTitle()}
                     </Typography>
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 1.5 }}>
+                        {/* Search Bar */}
+                        <Box sx={{
+                            position: 'relative',
+                            borderRadius: theme.shape.borderRadius,
+                            backgroundColor: alpha(theme.palette.common.white, 0.15),
+                            '&:hover': {
+                                backgroundColor: alpha(theme.palette.common.white, 0.25),
+                            },
+                            marginRight: theme.spacing(2),
+                            marginLeft: 0,
+                            width: 'auto',
+                            display: { xs: 'none', sm: 'flex' }
+                        }}>
+                            <Box sx={{ padding: theme.spacing(0, 1), height: '100%', position: 'absolute', pointerEvents: 'none', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                <SearchIcon />
+                            </Box>
+                            <InputBase
+                                placeholder="Search…"
+                                sx={{
+                                    color: 'inherit',
+                                    '& .MuiInputBase-input': {
+                                        padding: theme.spacing(1, 1, 1, 0),
+                                        paddingLeft: `calc(1em + ${theme.spacing(3)})`,
+                                        transition: theme.transitions.create('width'),
+                                        width: '12ch',
+                                        '&:focus': {
+                                            width: '20ch',
+                                        },
+                                    },
+                                }}
+                            />
+                        </Box>
+
+                        {/* Email Icon */}
+                        <IconButton color="inherit">
+                            <MailOutlineOutlinedIcon />
+                        </IconButton>
+
+                        {/* Notification Icon */}
+                        <IconButton color="inherit">
+                            <NotificationsNoneOutlinedIcon />
+                        </IconButton>
+
+                        {/* Avatar */}
+                        <Avatar sx={{ width: 32, height: 32, bgcolor: 'primary.light', ml: 1 }}>
+                            U
+                        </Avatar>
+
+                        {/* Username */}
+                        <Typography variant="subtitle1" noWrap component="div" sx={{ display: { xs: 'none', sm: 'block' } }}>
+                            Usuário Exemplo
+                        </Typography>
+                    </Box>
                 </Toolbar>
             </AppBar>
             <Box
