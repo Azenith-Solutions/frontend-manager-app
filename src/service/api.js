176107453dee3514cc8a97465a89fbac7c5b89ef
Polastrine <diogopolastrineds@gmail.com>
2025-09-28 22:36:37 -0300

    feat: criando dockerfile, yml e configuracao do nginx para servir arquivos estaticos

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_URL_BASE = import.meta.env.VITE_API_URL_BASE || "http://localhost:8080/api/v1";
+
 export const api = axios.create({
-    baseURL: "http://localhost:8080/api/v1",
+    baseURL: API_URL_BASE,
 });
 
 api.interceptors.request.use((config) => {
